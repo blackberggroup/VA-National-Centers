2a0be0764d13d0bbe780d6eeebf2023b747fb7a6
Ronald Ponferrada <ronaldponferrada@Ronalds-2023-MacBook-Pro.local>
2023-06-27 12:07:14 -0700

    adding Ronald's branch - Awards section

--- _site/assets/js/functions.js
+++ _site/assets/js/functions.js
@@ -1,6 +1,5 @@
 $('.vha-video__play-action').magnificPopup({
-    type: 'iframe',
-    
+    type: 'iframe', 
     
     iframe: {
        markup: '<div class="mfp-iframe-scaler">'+
@@ -9,5 +8,31 @@ $('.vha-video__play-action').magnificPopup({
                   '<div class="mfp-title">Some caption</div>'+
                 '</div>'
     }
-    
+});
+
+
+$(function () {
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    responsive:{
+      0: {
+        items: 2,
+        nav: true
+      },
+      800: {
+        items: 3,
+        nav: false
+      },
+      1000: {
+        items: 5,
+        nav: true,
+        loop: true
+      }
+    }
   });
+});
+
+
+
