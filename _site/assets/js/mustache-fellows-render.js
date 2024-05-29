a86984e1df8d5729bf355717f1c149055f8b64a3
ecstech-admin <james.strommer@ecstech.com>
2024-05-29 14:51:02 -0700

    feat: add news and events section

---
+++ _site/assets/js/mustache-fellows-render.js
@@ -0,0 +1,241 @@
+// JSON objects
+let news1 = {
+    url: "https://www.fedscoop.com/va-health-incubator-starts-disease-prevention-tools/",
+    fellow_status: "current",
+    img: "assets/img/home/news/va-health-incubator.png",
+    alt: "Surgeon in OR",
+    title: "VA health incubator starts with disease prevention tools",
+    article_copy: "VA's new collaborative health care innovation center is initially focusing on the creation of prevention tools that predict disease and improve patient safety."
+};
+
+let news2 = {
+    url: "https://distilgovhealth.com/2019/10/03/va-launches-predictive-analytics-clinical-decision-support-center/#:~:text=The%20Department%20of%20Veterans%20Affairs%20%28VA%29%20recently%20established,and%20clinical%20decision%20support%20to%20improve%20veteran%20care.",
+    fellow_status: "current",
+    img: "assets/img/home/news/predictive-analytics.png",
+    alt: "Man working on computer",
+    title: "VA Launches Predictive Analytics, Clinical Decision Support Center",
+    article_copy: "The Department of Veterans Affairs (VA) recently established the National Center for Collaborative Healthcare Innovation (NCCHI) within the Palo Alto Health Care System (VAPAHCS) in California, which will leverage predictive analytics, artificial intelligence, and clinical decision support to improve veteran care."
+};
+
+let news3 = {
+    url: "https://www.fedscoop.com/va-hospital-silicon-valley-5g-hololens-verizon-microsoft/",
+    fellow_status: "current",
+    img: "assets/img/home/news/project-convergence.png",
+    alt: "Veteran carrying American flag leading parade",
+    title: "Silicon Valley VA hospital gets taste of \'mixed reality\' through Project Convergence",
+    article_copy: "VA is building out an emerging technology program at a medical facility in Palo Alto, California, that has already put the VA on the map with its first \"5G hospital\"."
+};
+
+let news4 = {
+    url: "https://issuu.com/faircountmedia/docs/veterans_affairs_military_medicine_outlook_spring_/s/12248843",
+    fellow_status: "current",
+    img: "assets/img/home/news/personalized-healthcare.png",
+    alt: "Dr. Osborne in office with computer display of medical imaging examples",
+    title: "Innovative Technologies Support Personalized Healthcare",
+    article_copy: "One continuing characteristic of VA medical care is the embrace and application of innovative technologies to support the personalized healthcare of America's Veterans."
+};
+
+let news5 = {
+    url: "https://www.fedscoop.com/va-health-incubator-starts-disease-prevention-tools/",
+    fellow_status: "current",
+    img: "assets/img/home/news/disease-prevention-tools.jpg",
+    alt: "Excited group medical professionals holding up proof of 5G at hospital",
+    title: "VA health incubator starts with disease prevention tools",
+    article_copy: "VA's new collaborative health care innovation center is initially focusing on the creation of prevention tools that predict disease and improve patient safety."
+};
+
+let news6 = {
+    url: "https://www.usmedicine.com/non-clinical-topics/technology/vas-innovation-chief-sees-himself-as-a-catalyst-to-bring-people-ideas-together/",
+    fellow_status: "current",
+    img: "assets/img/home/news/bring-people-ideas-together.jpg",
+    alt: "",
+    title: "VA's Innovation Chief is Catalyst to Bring People, Ideas Together",
+    article_copy: "The journey of Thomas Osborne, MD, into medicine was not a straight line, but it was a path that seems strangely appropriate considering his role now as Director of VA's brand new National Center for Collaborative Healthcare Innovation."
+};
+
+let news7 = {
+    url: "https://tippie.uiowa.edu/news/business-analytics-capstone-project-finds-better-outcomes-covid-19-patients-low-dose-aspirin-regimen",
+    fellow_status: "current",
+    img: "assets/img/home/news/capstone-project.jpg",
+    alt: "",
+    title: "Business Analytics capstone project finds better outcomes for COVID-19 patients on low dose aspirin regimen",
+    article_copy: "Business Analytics capstone project finds better outcomes for COVID-19 patients on a low dose aspirin regimen"
+};
+
+let news8 = {
+    url: "https://blogs.va.gov/VAntage/85449/face-of-innovation-kalyn-essex/",
+    fellow_status: "current",
+    img: "assets/img/home/news/face-of-innovation.jpg",
+    alt: "Two women collaborating in front of computer",
+    title: "Face of Innovation: Kalyn Essex",
+    article_copy: "It's all about people for Kalyn Essex. Whether they be her fellow airmen, fellow Veterans, fellow employees, or her students, it's the connection she makes with people that she has always enjoyed - that human connection that led her to VA and eventually to becoming an Innovation Specialist at the VA Palo Alto Health Care System (HCS)."
+};
+
+let news9 = {
+    url: "https://ai.healthcareitnews.com/ai-powered-healthcare/va-launches-collaborative-healthcare-innovation-center-california",
+    fellow_status: "current",
+    img: "../assets/img/home/news/ncchi.jpg",
+    alt: "Doctors using VR tools",
+    title: "VA Launches Collaborative Healthcare Innovation Center in California",
+    article_copy: "With the goal of taking advantage of emerging technology to improve veteran care, the Department of Veterans Affairs (VA) recently established the National Center for Collaborative Healthcare Innovation (NCCHI) within the Palo Alto Health Care System (VAPAHCS) in California."
+};
+
+let news10 = {
+    url: "https://www.va.gov/opa/pressrel/pressrelease.cfm?id=5309",
+    fellow_status: "current",
+    img: "../assets/img/home/news/first-innovation-center.jpg",
+    alt: "First place trophy",
+    title: "VA pilots first collaborative health care innovation center in Palo Alto",
+    article_copy: "VA recently established the National Center for Collaborative Healthcare Innovation (NCCHI) within the VA Palo Alto Health Care System (VAPAHCS) in California as part of the department's strategy to advance Veteran health care services with cutting edge technology."
+};
+
+let news11 = {
+    url: "https://federalnewsnetwork.com/tom-temin-federal-drive/2019/11/va-creates-awards-innovation-honor-for-late-under-secretary-of-health/",
+    fellow_status: "current",
+    img: "../assets/img/home/news/robert-jesse-award.jpg",
+    alt: "",
+    title: "VA creates, awards innovation honor for late under secretary of Health",
+    article_copy: "To honor the late Under Secretary for Health Dr. Robert Jesse, the Department of Veterans Affairs named an annual award for Excellence in Innovation in his name. This year, the award went to Dr. Thomas Osborne, director of the VA National Center for Collaborative Healthcare Innovation."
+};
+
+let news12 = {
+    url: "https://www.digitaljournal.com/pr/4879783#ixzz7UA5UgnSb",
+    fellow_status: "current",
+    img: "../assets/img/home/news/smart-sock.jpg",
+    alt: "3D rendering of virus",
+    title: "Palarum Announces US Veterans Affairs to Evaluate Use of 'Smart Patient Sock' to Reduce Patient Falls",
+    article_copy: "VA and Palarum, LLC, announced that they have entered into a Cooperative Research and Development Agreement Program (CRADA) to evaluate Palarum's innovative e-wearable technology which is designed to reduce inpatient falls."
+};
+
+let news13 = {
+    url: "https://www.va.gov/opa/pressrel/pressrelease.cfm?id=5433",
+    fellow_status: "current",
+    img: "../assets/img/home/news/xprize-pandemic-alliance.jpg",
+    alt: "Rendering of circuit board",
+    title: "VA joins XPRIZE Pandemic Alliance to combat COVID-19 and future outbreaks",
+    article_copy: "VA announced that it has partnered with the XPRIZE Foundation's Pandemic Alliance as part of a public and private coalition to help develop ways to address the coronavirus disease 2019 (COVID-19) pandemic and subsequent global outbreaks."
+};
+
+let news14 = {
+    url: "https://med.stanford.edu/radiology/news/2019/thomas-osborne-received-robert-l-jesse-award.html",
+    fellow_status: "current",
+    img: "../assets/img/home/news/jesse-award-osborne.jpg",
+    alt: "Trophies in a trophy case",
+    title: "Dr. Osborne Received Inaugural Robert L. Jesse Award for Excellence in Innovation",
+    article_copy: "Thomas Osborne, MD, Clinical Assistant Professor (Affiliated) of Neuroimaging at the VA Palo Alto Health Care System (VAPAHCS), was presented the inaugural Under Secretary for Health Robert L. Jesse Award for Excellence in Innovation from VA. This award recognizes and honors VA employees who have demonstrated excellence and enabled the discovery and spread of health care innovation that exceeds expectations, restores hope and builds trust."
+};
+
+let news15 = {
+    url: "https://www.bloomberg.com/press-releases/2022-08-23/legacy-partners-with-the-us-department-of-veterans-affairs-to-analyze-fertility-in-global-war-on-terror-veterans",
+    fellow_status: "current",
+    img: "../assets/img/home/news/legacy-partners.jpg",
+    alt: "Woman looking through micrscope in lab",
+    title: "Legacy Partners with VA to Analyze Fertility in Global War On Terror",
+    article_copy: "Legacy, the leading digital male fertility clinic, announced its CRADA with VA to shed light on infertility in the military"
+};
+
+let news16 = {
+    url: "https://news.va.gov/99336/va-employee-revolutionizes-ridesharing-for-veterans/",
+    fellow_status: "current",
+    img: "../assets/img/home/news/veteran-rideshare.jpg",
+    alt: "Rideshare driver view",
+    title: "VA Employee Revolutionizes Ridesharing for Veterans",
+    article_copy: "Boston VA's Charles Franklin worked with Innovators Network and New England Center for Innovation Excellence to design and implement a rideshare program to meet Veteran housing and employment needs."
+};
+
+let news17 = {
+    url: "https://www.militarytimes.com/news/pentagon-congress/2022/08/23/study-to-examine-reasons-for-infertility-among-recent-war-veterans/",
+    fellow_status: "current",
+    img: "../assets/img/home/news/veteran-infertility.jpg",
+    alt: "Blurred photo of small group of doctors and nurses talking",
+    title: "Study to Examine Reasons for Infertility among Recent War Veterans",
+    article_copy: "Researchers announce plans for a new study to discover reasons why veterans from the recent wars are more likely to struggle with infertility than their civilian peers."
+};
+
+let news18 = {
+    url: "https://nationalcenterformobilitymanagement.org/blog/rideshare-veterans/",
+    fellow_status: "current",
+    img: "../assets/img/home/news/vulnerable-veterans-rideshare.jpg",
+    alt: "Rideshare driver placing items in trunk of car",
+    title: "Rideshare Program Helps Veterans Access Needed Services",
+    article_copy: "Studies have found that access to reliable transportation is one of the biggest factors when it comes to escaping from poverty. To meet the needs of unsheltered veterans and those at risk of homelessness, VA has launched a nationwide rideshare program to streamline vulnerable veterans' access to critically needed services."
+};
+
+let news19 = {
+    url: "https://news.va.gov/102631/rideshare-program-helps-veteran-find-news-roads-to-employment/",
+    fellow_status: "current",
+    img: "../assets/img/home/news/rideshare-for-veteran-employment.jpg",
+    alt: "Using smartphont to request rideshare",
+    title: "Rideshare Program Helps Veterans Find New Roads to Employment",
+    article_copy: "Despite trying to find employment near her apartment, Peters found no positions that could leverage her skillset that were within walking distance from her home. Although she preferred accomplishing tasks on her own, she was willing to work with VA on one thing: a ride to her “graveyard” shift."
+};
+
+
+// Mustache render logic for individual page views | ?user="firstname-lastname"
+var fellowsArray = new Array(news1, news2, news3, news4, news5, news6, news7, news8, news9, news10, news11, news12, news13, news14, news15, news16, news17, news18, news19);
+
+var tab;
+var img;
+var alt;
+var title;
+var article_copy;
+
+for (var i = 0; i < fellowsArray.length; i++) {
+    var userIndex = fellowsArray[i].url;
+    if (window.location.href.indexOf(userIndex) > -1) {
+
+        var objectView = fellowsArray[i];
+
+        img = Mustache.render("{{{img}}}", objectView);
+        alt = Mustache.render("{{alt}}", objectView);
+        bio_name = Mustache.render("{{bio_name}}", objectView);
+        title = Mustache.render("{{title}}", objectView);
+        article_copy = Mustache.render("{{{article_copy}}}", objectView);
+
+    }
+}
+
+
+// Append json data to html elements
+$('head title').text(tab);
+$('.bio-card-img > img').attr("alt", alt);
+$('.bio-card-attributes > h2').text(title);
+$('.bio-card-attributes > p span.article_copy').text(article_copy);
+
+$(".bio-card-attributes p span:empty").remove();
+$(".bio-card-attributes p:empty").remove();
+
+if ($(".bio-card-attributes > p.margin-top-0").text() == ' ') {
+    $(".bio-card-attributes > p.margin-top-0").remove();
+}
+
+if ($(".bio-card-attributes > p.margin-bottom-0").text() == ' ') {
+    $(".bio-card-attributes > p.margin-bottom-0").remove();
+}
+
+$("#bio-card-about p:empty").remove();
+
+
+// Render dynamic owl carousel for CURRENT sif & erf fellows
+var currentFellowsArray = fellowsArray;
+var newCurrentFellowsArray = [];
+
+for (var i = 0; i < currentFellowsArray.length; i++) {
+    if (currentFellowsArray[i].fellow_status == "current") {
+        newCurrentFellowsArray.push(currentFellowsArray[i]);
+        // console.log(newCurrentFellowsArray);
+    }
+}
+
+var currentData = {
+    data: []
+};
+
+currentData.data.push(newCurrentFellowsArray);  
+
+currentData.data[0].forEach(current => $('.owl-carousel.nci2i--news').append("<div class='usa-card__container item'>" + "<a href=" 
++ current.url + " />" + "<div class='usa-card__media' >" + "<div class=''>" + "<img src='" 
++ current.img + "'alt='" + current.alt + "' />" + "</div>" + "</div>" 
++ "<div class='usa-card__body'>" + "<h3 class='margin-y-0 h4'>" 
++ current.title + "</h3>" + "<p>" 
++ current.article_copy + "</p>" + "</div>" + "</a>" + "</div>"));
--- assets/js/functions.js
+++ assets/js/functions.js
@@ -144,6 +144,30 @@ $('.scroll').on('click',function(e) {
 	});
 });
 
+// if ($('.owl-carousel.current--fellows').length > 0) {
+//     $('.owl-carousel.current--fellows').owlCarousel({
+if ($('.owl-carousel.nci2i--news').length > 0) {
+    $('.owl-carousel.nci2i--news').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            },
+            1400:{
+                items:4
+            }
+        }
+    });
+}
+
 
 // Accessibility functions for owl-carousel plugin
 $( document ).ready(function() {
