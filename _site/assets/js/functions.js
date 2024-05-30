c22562ec4cff1020ad472477fbac0395c0f4176c
ecstech-admin <james.strommer@ecstech.com>
2024-05-30 14:06:40 -0700

    feat: hero carousel test

--- _site/assets/js/functions.js
+++ _site/assets/js/functions.js
@@ -38,8 +38,6 @@ if ($('.ncchi-home .owl-carousel').length > 0){
 	});
   }
 
-
-
 if ($('#drhci-body .owl-carousel').length > 0){
 	$('#drhci-body .owl-carousel').owlCarousel({
 		loop:true,
@@ -60,25 +58,55 @@ if ($('#drhci-body .owl-carousel').length > 0){
 	});
 }
 
-if ($('#nci2i-body .owl-carousel').length > 0){
-	$('#nci2i-body .owl-carousel').owlCarousel({
-		loop:true,
-		margin:10,
-		nav:true,
-		dots:false,
+// NCI2I hero carousel
+if ($('#test .owl-carousel').length > 0){
+	$(function () {
+	  $('#test .owl-carousel').owlCarousel({
+		loop: true,
+		margin: 10,
+		responsiveClass: true,
 		responsive:{
-			0:{
-				items:1
-			},
-			768:{
-				items:2
-			},
-			1400:{
-				items:4
-			}
+		  0: {
+			items: 1,
+			nav: true
+		  },
+		  800: {
+			items: 1,
+			nav: false
+		  },
+		  1000: {
+			items: 1,
+			nav: true,
+			loop: true
+		  }
 		}
+	  });
 	});
-}
+  }
+
+// if ($('.owl-carousel.current--fellows').length > 0) {
+//     $('.owl-carousel.current--fellows').owlCarousel({
+	if ($('.owl-carousel.nci2i--news').length > 0) {
+		$('.owl-carousel.nci2i--news').owlCarousel({
+			loop: true,
+			margin: 10,
+			nav: true,
+			responsive:{
+				0:{
+					items:1
+				},
+				600:{
+					items:2
+				},
+				1000:{
+					items:3
+				},
+				1400:{
+					items:4
+				}
+			}
+		});
+	}
 
 
 if ($('.owl-stage').length > 0) {
@@ -163,31 +191,6 @@ $('.scroll').on('click',function(e) {
 		// window.location.hash = target;
 	});
 });
-
-// if ($('.owl-carousel.current--fellows').length > 0) {
-//     $('.owl-carousel.current--fellows').owlCarousel({
-if ($('.owl-carousel.nci2i--news').length > 0) {
-    $('.owl-carousel.nci2i--news').owlCarousel({
-        loop: true,
-        margin: 10,
-        nav: true,
-        responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:2
-            },
-            1000:{
-                items:3
-            },
-            1400:{
-                items:4
-            }
-        }
-    });
-}
-
 
 // Accessibility functions for owl-carousel plugin
 $( document ).ready(function() {
