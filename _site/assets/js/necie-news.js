804455cec9b5333bf0884410624bed5af59ad15e
RyanCrux <ryan.loper@thecruxfirm.com>
2024-09-10 13:14:02 -0400

    feat: update owl links

--- _site/assets/js/necie-news.js
+++ _site/assets/js/necie-news.js
@@ -7,11 +7,11 @@ var newsData = {
             "title": "NECIE Identifies Innovations at VHA Shark Tank",
             "description": "During the 2022 edition of VHA Shark Tank competition, Leandro DaSilva, Director of the New England Center for Innovation of Excellence, served as a judge to identify employee-led health care solutions that promote positive outcomes and improved experiences for Veterans and VHA community.",
             "tag": "",
-            "url": "https://telehealth.va.gov/"
+            "url": ""
         },
         {
             "img": "../assets/img/necie/news/transforming-transportation-for-homeless-veterans.jpg",
-            "alt": "Man holding USA flag leading a group of people walking on the street",
+            "alt": "Man driving in a car",
             "title": "Transforming Transportation for Homeless Veterans ",
             "description": "The VA Rideshare Program, launched in 2018 at VA Boston HCS, offers transportation for homeless Veterans. Partnering with VHA IE and NECIE, it has provided over 250,000 rides since August 2021. Operating nationally, it supports healthcare, housing, food, employment, and disaster relief efforts.",
             "tag": "",
@@ -19,7 +19,7 @@ var newsData = {
         },
         {
             "img": "../assets/img/necie/news/mobius-mobility.jpg",
-            "alt": "HELPP logo graphic",
+            "alt": "Man sitting in wheelchair",
             "title": "Mobius Mobility",
             "description": "The IBOT Personal Mobility Device offers disabled Veterans freedom and independence with four driving modes for climbing curbs, navigating various terrains, interacting at eye level, and climbing stairs. NECIE, with Mobius Mobility, is delivering iBOTs and training to 25 VA SCI/D Centers and donating 25 iBOTs to Veterans.",
             "tag": "",
@@ -27,36 +27,39 @@ var newsData = {
         },
         {
             "img": "../assets/img/necie/news/move-the-sample.jpg",
-            "alt": "A woman standing and laptop showing feet sensor display",
+            "alt": "Laboratory test removed from box and organized on table",
             "title": "Move the Sample, Not the Patient",
             "description": "NECIE is collaborating with VA Puget Sound Health Care System and Tasso Inc., a Seattle based health startup focused on delivering at-home diagnostic testing. The collaboration will allow for a pilot to evaluate the efficacy of laboratory tests from samples obtained at home.",
             "tag": "",
-            "url": "https://news.va.gov/125818/connecting-veterans-to-diabetes-care-telehealth/"
+            "url": ""
         },
         {
             "img": "../assets/img/necie/news/give.jpg",
             "title": "Give Legacy, Inc.",
-            "alt": "Feet standing on sensor mat",
+            "alt": "person sitting with folded hands",
             "description": "VHA has partnered with Give Legacy, Inc. to test and cryopreserve sperm for Global War on Terror Veterans, addressing higher infertility rates compared to civilians. Researchers are collecting and analyzing samples, exploring factors like burn pit exposure, PTSD, and traumatic brain injury.",
             "tag": "",
-            "url": "https://marketplace.va.gov/innovations/reducing-hospital-admissions-and-amputation-prevention-remote-temperature-monitoring"
+            "url": ""
         },
         {
             "img": "../assets/img/necie/news/necie-wins-va-center-for-development.jpg",
+            "alt": "5 people receiving award on stage",
             "title": "NECIE Wins VA Center for Development and Civic Engagement (CDCE) Spirit Corporate Recruitment Award 2022",
             "description": "CDCE's American Spirit and Excellence Awards recognize outstanding programs, community partners, and staff for successful volunteer recruitment, impactful programs, and the dedication and talent of VA professionals within VHA and the Department.",
             "tag": "",
-            "url": "https://www.innovation.va.gov/hil/assets/documents/OHIL_State_of_Innovation_Report_2023.pdf#page=16"
+            "url": ""
         },
         {
             "img": "../assets/img/necie/news/hero.jpg",
+            "alt": "HeRO logo",
             "title": "NECIE Program Wins 2021 HeRO Awardees Non-Clinical Team from a VISN or Facility for Excellence in CARE and HRO Principles.",
             "description": "VA recognizes National HeRO Award recipients for their contributions to VHA's journey to becoming a high reliability organization (HRO). VHA's enterprise-wide journey to high reliability aims to ensure every patient receives excellent care every time.",
             "tag": "",
-            "url": "https://news.va.gov/121983/smartmat-delivers-preventative-telepodiatry"
+            "url": ""
         },
         {
             "img": "../assets/img/necie/news/soldierstrong.jpg",
+            "alt": "SoldierStrong logo",
             "title": "Operation Mobility: Cross-Country Tour to Donate 25 iBOT PMDs to Veterans",
             "description": "SoldierStrong, a nonprofit aiding Veterans with revolutionary medical technologies, and Dean Kamen of DEKA Research launched Operation Mobility, a 2.5-month, cross-country bus tour to donate 25 iBOT® PMDs to veterans.",
             "tag": "",
@@ -64,6 +67,7 @@ var newsData = {
         },  
         {
           "img": "../assets/img/necie/news/tech-wheelchair.jpg",
+          "alt": "Man in wheelchair going up stairs",
           "title": "Disabled Veteran from New Jersey Given High-Tech Wheelchair that Can Go Up Stairs",
           "description": "“I spent my whole life building medical equipment. I spent years building the iBot to restore dignity, access, balance to somebody that can’t walk. It’s a medical device,” Kamen says.",
           "tag": "",
